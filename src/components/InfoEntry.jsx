d625560377c36c963ee8616b07f2d14e3e4924a1
Steven Busenbarrick <sbusenba@gmail.com>
2022-06-12 16:24:07 -0700

    Add InfoEdit and InfoEntry components

---
+++ src/components/InfoEntry.jsx
@@ -0,0 +1,28 @@
+import { Component } from 'react';
+import '../styles/infoEntry.css'
+
+class InfoEntry extends Component {
+constructor(props) {
+    super(props)
+    this.props = props;
+  } 
+render(){
+return (
+    <div className="cv-form-div">
+      <legend>Personal Information:
+        <label htmlFor="fname">First Name:</label>
+        <input type="text" id="fname"></input>
+        <label htmlFor="lname">Last Name:</label>
+        <input type="text" id="lname"></input>
+        <label htmlFor="email">E-mail Address:</label>
+        <input type="text" id="email"></input>
+        <label htmlFor="phone">Phone Number:</label>
+        <input type="text" id="phone"></input>
+        <input type="button" id="infoSubmitButton" onClick={this.props.submitFunction}></input>
+      </legend>
+    </div>
+    );
+    }
+
+}
+export default InfoEntry;
