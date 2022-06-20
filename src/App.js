8aa96aef8486d4f7d381280af6f07d53c2cc7cac
Steven Busenbarrick <sbusenba@gmail.com>
2022-06-20 13:29:46 -0700

    Add phase variable to track state of CV entry.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 import InfoEntry from './components/InfoEntry';
+import SchoolEntry from './components/SchoolEntry'
+import SchoolEdit from './components/SchoolEdit'
 import InfoEdit from './components/InfoEdit'
 import { Component } from 'react';
 
 class App extends Component {
    constructor(){
     super();
-    this.state = {infoEntered:false,info:{first:'TestName',last:'TestNameLast',email:'dunno@duncare.com',phone:"(123)345-6789"}}
+    this.state = {
+      phase : 0, 
+      schoolInfo : [],
+      jobInfo: [],
+      infoEntered:false,
+      info:{first:'TestName',last:'TestNameLast',email:'dunno@duncare.com',phone:"(123)345-6789"}}
     
   }
   
@@ -19,6 +26,7 @@ class App extends Component {
     let info = {first: firstName.value,last: lastName.value, email: email.value, phone:phone.value}
     this.setState({info})
     this.setState({infoEntered:true})
+    this.setState({phase:1})
   }
   editInfo = ()=>{
     let firstName = document.getElementById('editfname') ;
@@ -28,6 +36,31 @@ class App extends Component {
     let info = {first: firstName.value,last: lastName.value, email: email.value, phone:phone.value}
     this.setState({info})
   }
+  getSchool = ()=>{
+    let school = document.getElementById('school') ;
+    let degree = document.getElementById('degree') ;
+    let startDate = document.getElementById('start-date') ;
+    let endDate = document.getElementById('end-date') ;
+    let schoolInfo = Array.from(this.state.schoolInfo);
+    schoolInfo.push({school: school.value,degree: degree.value, startDate: startDate.value, endDate:endDate.value})
+    this.setState({schoolInfo})
+    this.setState({phase:3})
+  }
+  getAnotherSchool = ()=>{
+    let school = document.getElementById('school') ;
+    let degree = document.getElementById('degree') ;
+    let startDate = document.getElementById('start-date') ;
+    let endDate = document.getElementById('end-date') ;
+    let schoolInfo = Array.from(this.state.schoolInfo);
+    schoolInfo.push({school: school.value,degree: degree.value, startDate: startDate.value, endDate:endDate.value})
+    this.setState({schoolInfo})
+  }
+
+
+  editSchool = (schoolInfo) => {
+    this.setState({schoolInfo})
+  }
+
   render(){
   return (
     <div className="App">
@@ -35,9 +68,12 @@ class App extends Component {
         <img src={logo} className="App-logo" alt="logo" />
     
         
-          {(!this.state.infoEntered)?
-          <InfoEntry submitFunction={this.getInfo}/>:
-          <InfoEdit submitFunction={this.editInfo} info = {this.state.info}/>}
+          {(this.state.phase === 0)? <InfoEntry submitFunction={this.getInfo}/>: ''}
+          {(this.state.phase === 1)? <SchoolEntry addAnotherFunction ={this.getAnotherSchool} submitFunction={this.getSchool} schools={this.state.schoolInfo}/>: ''}
+          
+          {(this.state.phase === 4)? <InfoEdit submitFunction={this.editInfo} info = {this.state.info}/>:''}
+          {(this.state.phase === 5)? <SchoolEdit submitFunction = {this.editSchool} schools = {this.state.schoolInfo}/>:''}
+          
       </header>
     </div>
   );
