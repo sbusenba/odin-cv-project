e681c5b214e465fd7c81e6ed7adadaa528a127e9
Steven Busenbarrick <sbusenba@gmail.com>
2022-06-27 10:14:45 -0700

    Create CVViewer.css to add styles to CVViewer

--- src/components/CVViewer.jsx
+++ src/components/CVViewer.jsx
@@ -1,6 +1,8 @@
 import SchoolView from "./SchoolView";
 import JobView from "./JobView"
 import { Component } from "react";
+import "../styles/CVViewer.css"
+
 class CVViewer extends Component{
 constructor(props){
     super(props)
@@ -15,16 +17,21 @@ render(){
         <div className="cv-view">
             <div className="personal-info">
                 <h3>{this.info.first+' '+this.info.last}</h3>
-                <div className="contact-info"></div>
+                <div className="contact-info">
                 <p>{this.info.email}</p>
                 <p>{this.info.phone}</p>
-            
+                </div>
             </div>
             <div className="education">
-                {this.schoolInfo.map((school)=><SchoolView school={school}/>)}
+                <h3>Education</h3>
+                {this.schoolInfo.map((school)=><SchoolView 
+                key={school.school+school.endDate} 
+                school={school}/>)}
             </div>
             <div className="experience">
-                {this.jobInfo.map((job)=><JobView job={job}/>)}
+                <h3>Experience</h3>
+                {this.jobInfo.map((job)=><JobView 
+                key={job.company+job.endDate} job={job}/>)}
             </div>
 
         </div>
--- src/components/SchoolView.jsx
+++ src/components/SchoolView.jsx
@@ -6,12 +6,12 @@ this.school = props.school;
 }
 render(){
     return(
-    <div className="school-view" key={this.school+this.endDate}>
+    <div className="school-view">
         <div className='school-text'>
             <h4>{this.school.school}</h4>
             <p>{this.school.degree}</p>
         </div>
-        <div className='school-text'>
+        <div className='school-dates'>
             <p>{this.school.startDate}</p>
             <p>{this.school.endDate}</p>
         </div>
